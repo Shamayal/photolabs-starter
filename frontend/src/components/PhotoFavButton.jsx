9348f6d7bb84cae3c65cee8078cadbc9d2e5d667
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-20 02:01:50 -0400

    added functionality to allow user to click like

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -2,14 +2,27 @@ import React, { useCallback, useState } from 'react';
 
 import { FavIcon } from './FavIcon';
 import '../styles/PhotoFavButton.scss';
+import FavBadge from './FavBadge';
 
 function PhotoFavButton() {
+
+  const [like, setLike] = useState("unliked");
+
+  const handleClick = () => {
+    setLike((like === "liked") ? "unliked" : "liked");
+  }
+  // console.log("button state is " + like)
+
+
   return (
-    <div className="photo-list--fav-icon">
-      <div className="photo-list--fav-icon-svg">
-        {/* Insert React */}
-      </div>
-    </div>
+    // <div className="photo-list--fav-icon">
+    //   <div className="photo-list--fav-icon-svg">
+    //     {/* Insert React */}
+    //   </div>
+    // </div>
+    <button onClick={handleClick} className="photo-list--fav-icon">
+      {like === "liked" ? <FavBadge /> : <FavIcon />}
+    </button>
   );
 }
 
