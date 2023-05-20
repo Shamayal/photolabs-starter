a82470f2661fbbf5c97c09575f1ef24d77db238b
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-20 06:03:17 -0400

    added topics and heart icon to nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import './App.scss';
 import PhotoFavButton from './components/PhotoFavButton';
 import PhotoList from './components/PhotoList';
 import TopicList from './components/TopicList';
+import TopNavigationBar from './components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigationBar />
       <PhotoList />
       <PhotoFavButton />
     </div>
