6a5af0d7d8672205f929845a570f8fd2e8dd0356
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-20 05:39:38 -0400

    added topic list component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoFavButton from './components/PhotoFavButton';
 import PhotoList from './components/PhotoList';
+import TopicList from './components/TopicList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   
   return (
     <div className="App">
+      <TopicList />
       <PhotoList />
       <PhotoFavButton />
     </div>
