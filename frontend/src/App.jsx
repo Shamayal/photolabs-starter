ad53ffc17cc7c5ebeb31363a9dee279b0b3af5ef
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-20 12:21:33 -0400

    imported photos and topics and passed down props to TopNavigationBar and PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,14 @@ import PhotoList from './components/PhotoList';
 import TopNavigationBar from './components/TopNavigationBar';
 import HomeRoute from './routes/HomeRoute';
 
+import photos from './mocks/photos.json';
+import topics from './mocks/topics.json';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute photos={photos} topics={topics}/>
       {/* <TopNavigationBar />
       <PhotoList />
       <PhotoFavButton /> */}
