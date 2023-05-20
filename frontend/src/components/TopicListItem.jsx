574ab4be20666d24921af7785128dfcfbf2e5423
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-20 14:28:35 -0400

    spacing in props

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -4,7 +4,7 @@ import '../styles/TopicListItem.scss'
 
 const TopicListItem = (props) => {
 
-  const { link, title } = props;
+  const { link , title } = props;
 
   return (
     <div className="topic-list--item">
