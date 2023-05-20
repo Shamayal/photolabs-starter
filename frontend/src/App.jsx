bef3f8735d0c61d1b02dd18ee5504f6b2507e60e
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-20 11:35:16 -0400

    added top navigation bar and photos section to HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 import './App.scss';
 import PhotoFavButton from './components/PhotoFavButton';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
 import TopNavigationBar from './components/TopNavigationBar';
+import HomeRoute from './routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   
   return (
     <div className="App">
-      <TopNavigationBar />
+      <HomeRoute />
+      {/* <TopNavigationBar />
       <PhotoList />
-      <PhotoFavButton />
+      <PhotoFavButton /> */}
     </div>
   )
 }
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
 
 const HomeRoute = () => 
   <div className="home-route">
-    {/* Insert React */}
+    <TopNavigationBar />
+    <PhotoList />
   </div>
 
 export default HomeRoute;
