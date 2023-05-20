dc003ef1775de5f7b01bebf8c8b1ed27351acd45
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-20 04:23:35 -0400

    built the photo list container with map to render images

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
 import '../styles/PhotoList.scss';
+import PhotoListItem from './PhotoListItem';
 
-const PhotoList = () => {
-  <ul className="photo-list">
-    {/* Insert React */}
-  </ul>
+const PhotoList = (props) => {
+
+  const photosParsedComponents = props.photos.map((photo, index) => {
+    return (
+      <PhotoListItem 
+        key={index}
+      />
+    )
+  })
+
+  return (
+    <ul className="photo-list">
+      {photosParsedComponents}
+    </ul>
+  )
 }
 
 PhotoList.defaultProps = {
