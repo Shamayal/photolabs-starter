54879590c6b13f847bc1a2f8c34f62e25a175a7e
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-21 11:04:37 -0400

    added state and button function for opening modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 import HomeRoute from './routes/HomeRoute';
 
 import photos from './mocks/photos.json';
 import topics from './mocks/topics.json';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  const [photoOpen, setPhotoOpen] = useState(false);
+  const [clickPhotoId, setClickPhotoId] = useState(null);
+
+  const photoClick = (photoId) => {
+    setPhotoOpen(true);
+    setClickPhotoId(photoId)
+    console.log("clicked on photo id: ", photoClick)
+  }
   
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics}/>
+      <HomeRoute photos={photos} topics={topics} photoClick={photoClick}/>
+      {photoOpen && <PhotoDetailsModal photoClick={photoClick}/>}
     </div>
   )
 }
