7efc88256c031f59393ae5e59d2f400b2d999c91
Shamayal Syed <shamayalsyed9@gmail.com>
2023-05-20 23:11:14 -0400

    display a notification for liked photos

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -6,13 +6,13 @@ import FavBadge from './FavBadge';
 
 const TopNavigation = (props) => {
 
-  const { topics } = props;
+  const { topics , likedPhotos } = props;
 
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar--logo">PhotoLabs</span>
       <TopicList topics={topics}/>
-      <FavBadge />
+      <FavBadge isFavPhotoExist={likedPhotos.length >= 1 ? true : false}/>
     </div>
   )
 }
